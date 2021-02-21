073317ab6c8d4f735717d0bb01a31a32a299342c
Ryan Shtirmer <ryan@Ryans-MacBook-Air.local>
2021-02-21 05:54:17 -0500

    begin pg support

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ let pg_client;
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   pg_client = await pool.connect();
-  const result = await pg_client.query('CREATE TABLE IF NOT EXISTS counter (id integer UNIQUE, count integer);');
+  const result = await pg_client.query('CREATE TABLE IF NOT EXISTS counter (id text UNIQUE, count integer);');
 });
 
 client.on('message', async msg => {
-  //INSERT INTO counter (id, count) VALUES (1, -1) ON CONFLICT (id) DO NOTHING;
-  //UPDATE counter SET count = count + 1 WHERE id = 1;
-  //SELECT count FROM test WHERE id = 1;
-  let authorID = msg.author.id
-  await pg_client.query('INSERT INTO counter (id, count) VALUES ($1, -1) ON CONFLICT (id) DO NOTHING;', [authorID])
+  let authorID = msg.author.id.toString()
+  await pg_client.query('INSERT INTO counter (id, count) VALUES ($1, 0) ON CONFLICT (id) DO NOTHING;', [authorID])
   await pg_client.query('UPDATE counter SET count = count + 1 WHERE id = $1;', [authorID])
 
   if (msg.content === '!postcount') {
